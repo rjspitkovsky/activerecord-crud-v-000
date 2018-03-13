43f3ffbd4f7ffa8952f81ddc29d9f6a0a1199234
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-13 20:32:47 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -54,7 +54,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.find_by("release_date > 2002 ORDER BY release_date DESC")
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
@@ -83,7 +83,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   long_title = Movie.find_by(:title => "That One Where the Guy Kicks Another Guy Once")
-  long_title.destroy 
+  long_title.destroy
 end
 
 def can_destroy_all_items_at_once
